0c5a8d60dc21e0b94f2c03656b39ccdfd672a510
SiamKing <thesiameseelectric@gmail.com>
2018-09-18 14:37:43 -0700

    Added authentication links to navbar

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { DataStorageService } from '../recipes/shared/data-storage.service';
 import { Response } from '@angular/http';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
 
-  constructor(private dsService: DataStorageService) {}
+  constructor(private dsService: DataStorageService, private authService: AuthService) {}
+
+  ngOnInit() {
+  }
 
   onSaveData() {
     this.dsService.storeRecipes()
@@ -23,4 +27,9 @@ export class HeaderComponent {
   onFetchData() {
    this.dsService.getRecipes();
   }
+
+  onLogout() {
+    this.authService.logOut();
+  }
+
 }
